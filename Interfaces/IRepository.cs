9ff227a6d3a7ee2ebc0892f34df7512e407de293
KavinderSingh <kavindersingh42@gmail.com>
2023-09-14 13:28:46 +0530

    Add project files.

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace EntityFrameworkCore.RepositoryPattern.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T? Get(int id);
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+        Task Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(int id);
+        void RemoveRange(IEnumerable<T> entities);
+        Task<int> SaveAsync();
+        Task<int> Count(Expression<Func<T, bool>> expression);
+        void Update(T entity);
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,67 @@
+﻿using EntityFrameworkCore.RepositoryPattern.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EntityFrameworkCore.RepositoryPattern
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly dynamic _context;
+
+        public Repository(dynamic context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(T entity)
+        {
+            _ = await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+
+        public async Task<int> Count(Expression<Func<T, bool>> expression)
+        {
+            return await _context.Set<T>().CountAsync(expression);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+
+        public T? Get(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Remove(int id)
+        {
+            T? entityToDelete = _context.Set<T>().Find(id);
+            _ = _context.Set<T>().Remove(entityToDelete!);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
